40c7a2568d337f790909200624c8a32b81c4893e
prime.zeng <zhunbeifang2@outlook.com>
2022-08-20 07:51:31 +0800

    Start DDGI

---
+++ Code/Maple/src/Engine/Raytrace/RaytracedReflection.cpp
@@ -0,0 +1,20 @@
+//////////////////////////////////////////////////////////////////////////////
+// This file is part of the Maple Engine                              		//
+//////////////////////////////////////////////////////////////////////////////
+
+#include "RaytracedReflection.h"
+
+namespace maple
+{
+	namespace raytraced_shadow
+	{
+		namespace component
+		{
+
+		}
+
+		auto registerRaytracedReflection(ExecuteQueue &update, ExecuteQueue &queue, std::shared_ptr<ExecutePoint> executePoint) -> void
+		{
+		}
+	};        // namespace raytraced_shadow
+};            // namespace maple
---
+++ Code/Maple/src/Engine/Raytrace/RaytracedReflection.h
@@ -0,0 +1,25 @@
+//////////////////////////////////////////////////////////////////////////////
+// This file is part of the Maple Engine                              		//
+//////////////////////////////////////////////////////////////////////////////
+#pragma once
+#include "RHI/Texture.h"
+#include "RaytraceScale.h"
+#include "Scene/System/ExecutePoint.h"
+
+namespace maple
+{
+	namespace raytraced_shadow
+	{
+		namespace component
+		{
+			struct RaytracedReflecton
+			{
+				RaytraceScale::Id scale = RaytraceScale::Full;
+				uint32_t          width;
+				uint32_t          height;
+				Texture2D::Ptr    output;
+			};
+		}        // namespace component
+		auto registerRaytracedReflection(ExecuteQueue &update, ExecuteQueue &queue, std::shared_ptr<ExecutePoint> executePoint) -> void;
+	};        // namespace raytraced_shadow
+};            // namespace maple
